9c83ce3b3bc3fcd6bc75a6132647c5c072f4d75f
hakanturgay <turgayh@itu.edu.tr>
2021-02-23 01:15:48 +0300

    update redis helper

--- src/helper/redis-db.js
+++ src/helper/redis-db.js
@@ -3,5 +3,4 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const redisClient = redis.createClient(process.env.redis_connection)
-console.log(process.env.redis_connection);
 module.exports = { redisClient }
