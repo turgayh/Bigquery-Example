3f986d18374b5668c9b0812fec45b7f7aa973d82
hakanturgay <turgayh@itu.edu.tr>
2021-02-22 23:36:57 +0300

    update lint & redis  connection parameter

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 
 const morgan = require('morgan')
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const errorHandler = require('./src/middleware/error-handler');
-const { sendMessage } = require('./src/helper/pub-sub')
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const errorHandler = require('./src/middleware/error-handler')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(morgan('combined'))
 
 // api routes
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
-// 
-app.use('/api/v1/event', require('./src/event/event.controller'));
-app.use('/api/v1/crm/user-stat', require('./src/crm/user-stat.controller'));
-app.use('/api/v1/crm/daily-stat', require('./src/crm/daily-stat.controller'));
+
+app.use('/api/v1/event', require('./src/event/event.controller'))
+app.use('/api/v1/crm/user-stat', require('./src/crm/user-stat.controller'))
+app.use('/api/v1/crm/daily-stat', require('./src/crm/daily-stat.controller'))
 
 // global error handler
-app.use(errorHandler);
+app.use(errorHandler)
 
 // start server
 app.listen(process.argv[2] || process.env.PORT || 4500, () => {
-    console.log(`Uygulama ayakta ve ${process.argv[2] || process.env.PORT || 4500} nolu porttan dinlemede.`);
-});
+    console.log(`Uygulama ayakta ve ${process.argv[2] || process.env.PORT || 4500} nolu porttan dinlemede.`)
+})
--- src/helper/redis-db.js
+++ src/helper/redis-db.js
@@ -1,3 +1,7 @@
-const redis = require("redis");
-const redisClient = redis.createClient(process.env.redisConnectionString);
+const redis = require('redis')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const redisClient = redis.createClient(process.env.redis_connection)
+console.log(process.env.redis_connection);
 module.exports = { redisClient }
